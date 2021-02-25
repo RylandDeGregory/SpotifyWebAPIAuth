ff14f495d9023f58c02c48ecaacd7513ad6e174d
Ryland DeGregory <rylanddegregory@me.com>
2021-02-24 19:16:14 -0500

    Add /refresh_token endpoint

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -116,5 +116,29 @@ app.get('/callback', function(req, res) {
   }
 });
 
+app.get('/refresh_token', function(req, res) {
+
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  // Request an Access token from a Refresh token
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
+
 console.log('Listening on 8888');
 app.listen(8888);
